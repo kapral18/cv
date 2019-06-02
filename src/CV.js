2cf90544e2254a69a960a49fd8870cd6a2bd7100
Karen Grigoryan <karen@karenjs.com>
2019-06-02 23:30:18 +0200

    fix: ipsoft timeline (#4)

--- src/CV.js
+++ src/CV.js
@@ -2,4 +2,4 @@ import React from "react";
 import Doc from "./Document";
 import ReactPDF from "@react-pdf/renderer";
 
-ReactPDF.render(<Doc isPdf />, `${__dirname}/../out/cv.pdf`);
+ReactPDF.render(<Doc isPdf />, `${__dirname}/../docs/cv.pdf`);
