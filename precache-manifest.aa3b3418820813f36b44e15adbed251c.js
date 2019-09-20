df838b2e41583026152e37b65c433ca39d81711b
Karen Grigoryan <karen@karenjs.com>
2019-09-20 22:00:27 +0200

    Updates

---
+++ precache-manifest.aa3b3418820813f36b44e15adbed251c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fd244c40c55388b7e162ac26ef24e3",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "7c0976575ac6c39d5dee",
+    "url": "/cv/static/css/main.43e8434d.chunk.css"
+  },
+  {
+    "revision": "6526a1112ac9f210c2d9",
+    "url": "/cv/static/js/2.6526a111.chunk.js"
+  },
+  {
+    "revision": "ed055423170ec3bed8602fe3284c88ee",
+    "url": "/cv/static/js/2.6526a111.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c0976575ac6c39d5dee",
+    "url": "/cv/static/js/main.7c097657.chunk.js"
+  },
+  {
+    "revision": "b602b1e05cf1af7a1cad",
+    "url": "/cv/static/js/runtime~main.b602b1e0.js"
+  }
+]);
